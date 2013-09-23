d40853de613df3baf3502a098eb7b1d21b672a96
Enrico Mischorr <enrico@mischorr.de>
2013-09-23 19:33:16 +0200

    extend network node

--- src/de/micralon/engine/net/NetworkNode.java
+++ src/de/micralon/engine/net/NetworkNode.java
@@ -1,5 +1,8 @@
 package de.micralon.engine.net;
 
+import de.micralon.engine.Player;
+
 public interface NetworkNode {
 	public void sendMessage(Object message);
+	public Player getLocalPlayer();
 }
