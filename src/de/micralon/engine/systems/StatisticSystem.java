db15696800c49b7255213e0caadfb1d1eb0c93c2
Enrico Mischorr <enrico@Enrico-Mischorrs-iMac.local>
2013-09-25 13:49:44 +0200

    statistic system

---
+++ src/de/micralon/engine/systems/StatisticSystem.java
@@ -0,0 +1,62 @@
+package de.micralon.engine.systems;
+
+import com.badlogic.gdx.utils.ObjectMap;
+
+public class StatisticSystem {
+	private long levelStartTime;
+	private ObjectMap<Integer, Integer> playerPoints = new ObjectMap<Integer, Integer>();
+	private ObjectMap<Integer, Integer> playerKills = new ObjectMap<Integer, Integer>();
+	private ObjectMap<Integer, Integer> playerDeaths = new ObjectMap<Integer, Integer>();
+	
+	// temp vars
+	int levelSeconds;
+	
+	public void startLevel() {
+		levelStartTime = System.currentTimeMillis();
+	}
+	
+	public int getLevelTime() {
+		return (int) ((levelStartTime - System.currentTimeMillis()) / 1000);
+	}
+	
+	public String getLevelTimeAsString() {
+		levelSeconds = getLevelTime();
+		return String.valueOf(levelSeconds/60) + ":" + String.valueOf(levelSeconds%60);
+	}
+	
+	public ObjectMap<Integer, Integer> getPlayerPoints() {
+		return playerPoints;
+	}
+	
+	public int getPlayerPoints(int playerID) {
+		return playerPoints.get(playerID);
+	}
+	
+	public void addPoints(int playerID, int points) {
+		playerPoints.put(playerID, playerPoints.get(playerID)+points);
+	}
+	
+	public ObjectMap<Integer, Integer> getPlayerKills() {
+		return playerKills;
+	}
+	
+	public int getPlayerKills(int playerID) {
+		return playerKills.get(playerID);
+	}
+	
+	public void addKill(int playerID) {
+		playerKills.put(playerID, playerKills.get(playerID)+1);
+	}
+	
+	public ObjectMap<Integer, Integer> getPlayerDeaths() {
+		return playerDeaths;
+	}
+	
+	public int getPlayerDeaths(int playerID) {
+		return playerDeaths.get(playerID);
+	}
+	
+	public void addDeath(int playerID) {
+		playerDeaths.put(playerID, playerDeaths.get(playerID)+1);
+	}
+}
