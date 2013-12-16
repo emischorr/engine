66632cf4e933e692b19d14c963731363b2984e9a
Enrico Mischorr <enrico@Enrico-Mischorrs-iMac.local>
2013-12-16 16:03:10 +0100

    item class

---
+++ src/de/micralon/engine/Item.java
@@ -0,0 +1,5 @@
+package de.micralon.engine;
+
+public abstract class Item {
+	private float inventorySize; // we want to be flexible -> inventorySize could be storage room or weight -> as a float
+}
