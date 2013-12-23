0b97d0c9087fe3a0704297f9b6bbaf2165da58ba
Enrico Mischorr <enrico@Enrico-Mischorrs-iMac.local>
2013-12-23 09:24:00 +0100

    add super constructors

--- src/de/micralon/engine/DestructibleGameObject.java
+++ src/de/micralon/engine/DestructibleGameObject.java
@@ -1,8 +1,23 @@
 package de.micralon.engine;
 
+import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
+import com.badlogic.gdx.utils.Scaling;
+
 public abstract class DestructibleGameObject extends GameObject implements Destructible {
 	protected DamageModel damageModel;
 	protected boolean destroyed = false;
+	
+	public DestructibleGameObject() {
+		super();
+	}
+	
+	protected DestructibleGameObject(BodyType type, float bodyWidth, float bodyHeight, float linearDamping, float angularDamping) {
+		super(type, bodyWidth, bodyHeight, linearDamping, angularDamping);
+	}
+	
+	protected DestructibleGameObject(BodyType type, float bodyWidth, float bodyHeight, float linearDamping, float angularDamping, Scaling scaling) {
+		super(type, bodyWidth, bodyHeight, linearDamping, angularDamping, scaling);
+	}
 	
 	@Override
 	public DamageModel getDamageModel() {
