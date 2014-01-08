6b0de9f8fe639da6734c591631663edd0ffaa9d9
Enrico Mischorr <enrico@Enrico-Mischorrs-iMac.local>
2014-01-08 14:12:48 +0100

    return last position if body already destroyed

--- src/de/micralon/engine/GameObject.java
+++ src/de/micralon/engine/GameObject.java
@@ -36,6 +36,8 @@ public abstract class GameObject extends Image implements Reuseable {
 	protected float bodyWidth;
 	protected float bodyHeight;
 	
+	private Vector2 lastPos;
+	
 	// temp vars
 	private ObjectData data;
 	
@@ -126,7 +128,7 @@ public abstract class GameObject extends Image implements Reuseable {
 		if (body != null) {
 			return body.getPosition();
 		} else {
-			return null;
+			return lastPos;
 		}
 	}
 	
@@ -169,9 +171,11 @@ public abstract class GameObject extends Image implements Reuseable {
 	
 	/**
 	 * Don't call this directly from inside the physics step
+	 * WARNING: this destroys the body
 	 */
 	public void destroy() {
 		if (body != null) {
+			lastPos = body.getPosition();
 			GameWorld.ctx.box2dWorld.destroyBody(body);
 			body = null;
 		}
