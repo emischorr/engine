156e9746638ec9e9502642e117d0b8f9c8a5c678
Enrico Mischorr <enrico@Enrico-Mischorrs-iMac.local>
2014-01-28 13:32:11 +0100

    fix drop helper

--- src/de/micralon/engine/DropHelper.java
+++ src/de/micralon/engine/DropHelper.java
@@ -2,10 +2,12 @@ package de.micralon.engine;
 
 import java.util.Random;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.utils.Array;
 
 public class DropHelper {
 	public static final int DROP_BASE_VALUE = 100;
+	private static float chance;
 	
 	public static Array<Dropable> drop() {
 		return drop(DROP_BASE_VALUE, drops, false);
@@ -20,17 +22,23 @@ public class DropHelper {
 		if (drops != null) {
 			Random random = new Random();
 			for (Drop drop : drops) {
-				// calculate chance to drop this
-		 		float chance = (dropValue/DROP_BASE_VALUE/drop.valueLevel) * drop.minChance;
-				if (chance > drop.maxChance) chance = drop.maxChance; // limit chance
+				calcChance(dropValue, drop);
 				// do we have luck?
-				if (random.nextInt(100)/100f <= chance) {
+				if ( (random.nextInt(100)+1)/100f <= chance ) {
+					Gdx.app.log("DropHelper", "Got lucky ("+chance+"%). droping...");
 					items.add(drop.item);
 					if (!multipleDrop) break;
 				}
 			}
 		}
 		return items;
+	}
+	
+	protected static void calcChance(float dropValue, Drop drop) {
+		// calculate chance to drop this
+		chance = dropValue/(DROP_BASE_VALUE*drop.valueLevel);
+ 		if (chance < drop.minChance) chance = drop.minChance;
+		if (chance > drop.maxChance) chance = drop.maxChance; // limit chance
 	}
 	
 	public static Array<Drop> drops = new Array<Drop>();
@@ -44,20 +52,20 @@ public class DropHelper {
 	public static class Drop {
 		public Dropable item;
 		public float maxChance = 100;
-		public float minChance = 1;
+		public float minChance = 0;
 		public int valueLevel = 1;
 		
 		public Drop(Dropable item) {
 			this.item = item;
 		}
 		
-		public Drop(Dropable item, float maxChance, float minChance) {
+		public Drop(Dropable item, float minChance, float maxChance) {
 			this.item = item;
 			this.maxChance = maxChance;
 			this.minChance = minChance;
 		}
 		
-		public Drop(Dropable item, float maxChance, float minChance, int valueLevel) {
+		public Drop(Dropable item, float minChance, float maxChance, int valueLevel) {
 			this.item = item;
 			this.maxChance = maxChance;
 			this.minChance = minChance;
