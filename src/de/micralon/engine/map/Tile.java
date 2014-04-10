859f5e15bc3d5e0bdad21f3fcc19b51ac42ce1ca
Enrico Mischorr <enrico.mischorr@bechtle.com>
2014-04-10 16:39:06 +0200

    selection calculation in MapUtils

--- src/de/micralon/engine/map/Tile.java
+++ src/de/micralon/engine/map/Tile.java
@@ -2,6 +2,7 @@ package de.micralon.engine.map;
 
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.utils.Align;
 
@@ -26,6 +27,10 @@ public abstract class Tile extends Image {
 	
 	public String identifier() {
 		return tileX+"/"+tileY;
+	}
+	
+	public Vector2 getCoords() {
+		return new Vector2(tileX, tileY);
 	}
 	
 	public void drawDebug(ShapeRenderer shapeRenderer) {}
--- src/de/micralon/engine/utils/MapUtils.java
+++ src/de/micralon/engine/utils/MapUtils.java
@@ -1,6 +1,7 @@
 package de.micralon.engine.utils;
 
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.Array;
 
 import de.micralon.engine.map.IsoTile;
 
@@ -31,6 +32,53 @@ public class MapUtils {
 		temp.x = (float) (normalX - Math.floor((normalX + normalY)/2));
 		temp.y = normalX + normalY;
 		return temp;
+	}
+	
+	/**
+	 * Calculates coordinate-pairs belonging to the selection. <br/>
+	 * The selection is a rectangle based on the start and end point. <br/>
+	 * Coordinates are expected to represent tiles on a staggered isometric map. While the returned coordinates represent tiles in a Cartesian map.<br/>
+	 * Use {@link MapUtils#normalToStaggerd} to convert the result back to a staggered isometric representation.
+	 * @param start Coordinate
+	 * @param end Coordinate
+	 * @return an array of coordinates belonging to the selection rectangle (Cartesian map representation)
+	 */
+	public static Array<Vector2> getSelectionCoordsStaggered(Vector2 start, Vector2 end) {
+		return getSelectionCoords(MapUtils.staggeredToNormal(start), MapUtils.staggeredToNormal(end));
+	}
+	
+	/**
+	 * Calculates coordinate-pairs belonging to the selection. <br/>
+	 * The selection is a rectangle based on the start and end point. <br/>
+	 * Coordinates are expected to represent tiles on a Cartesian map.
+	 * @param start Coordinate
+	 * @param end Coordinate
+	 * @return an array of coordinates belonging to the selection rectangle
+	 */
+	public static Array<Vector2> getSelectionCoords(Vector2 start, Vector2 end) {
+		Array<Vector2> selection = new Array<Vector2>();
+		for (float x = start.x; hasNextTile(start.x, end.x, x); nextTile(start.x, end.x, x)) {
+			for (float y = start.y; hasNextTile(start.y, end.y, y); nextTile(start.y, end.y, y)) {
+				selection.add(new Vector2(x, y));
+			}
+		}
+		return selection;
+	}
+	
+	private static void nextTile(float start, float end, float a) {
+		if (start > end) {
+			a--;
+		} else {
+			a++;
+		}
+	}
+	
+	private static boolean hasNextTile(float start, float end, float a) {
+		if (start > end) {
+			return a > end;
+		} else {
+			return a < end;
+		}
 	}
 	
 }
